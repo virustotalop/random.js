d2a9b85c67830517a034610abd00159788c98688
virustotalop <virustotalop@gmail.com>
2018-10-13 22:54:05 -0700

    Add initial testing framework

--- test/test.py
+++ test/test.py
@@ -1,10 +1,20 @@
 import unittest
+import os
 from selenium import webdriver
 
+driver = webdriver.Firefox()
+
+file = "file://" + os.getcwd() + "/test.html"
+print(file)
+driver.get(file)
 
 
 
 class TestRandom(unittest.TestCase):
 
 	def test_next_int(self):
+		self.assertEqual(driver.execute_script("var rand = new Random(100); return rand.nextInt(100);"), 15) 
 		
+		
+
+unittest.main()
